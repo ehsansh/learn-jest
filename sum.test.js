46cfa61c2e98a05ef11b643947e9668901c659ba
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-03-21 14:56:43 +0330

    write a test for using toEqual

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,13 @@
 const sum = require('./sum.js');
 
-it('should add 2 to 3 to equal 5', () => {
-    const result = sum(2, 3);
-    expect(result).toBe(5);
+describe('example tests', () => {
+    it('should add 2 to 3 to equal 5', () => {
+        const result = sum(2, 3);
+        expect(result).toBe(5);
+    });
+
+    it('check object', () => {
+        const obj = {};
+        expect(obj).toEqual({});
+    });
 });
